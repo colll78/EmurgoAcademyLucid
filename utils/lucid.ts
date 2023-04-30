ef7435641b36ddaa327527159cdea30ac0ed80eb
colll78 <root@DESKTOP-8CUAVVE>
2023-04-30 12:04:34 -0500

    cip 68

--- utils/lucid.ts
+++ utils/lucid.ts
@@ -9,12 +9,12 @@ const initLucid = async (wallet: string) => {
     // const eternl = window.cardano.eternl.enable();
     // const gero = await window.cardano.gero.enable();
     
-    // const lucid = await Lucid.new(
-    //     new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', "preprodLT9nUTb22P26FiVH42jSFJU2IZaNxZVz") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
-    //     ,'Preprod')
     const lucid = await Lucid.new(
-            new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', "mainneto2wd71NAi5sZMWDHUTXxgvMTEC6ciS2I") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
-            ,'Mainnet')
+        new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', "preprodLT9nUTb22P26FiVH42jSFJU2IZaNxZVz") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
+        ,'Preprod')
+    // const lucid = await Lucid.new(
+    //         new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', "mainneto2wd71NAi5sZMWDHUTXxgvMTEC6ciS2I") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
+    //         ,'Mainnet')
     lucid.selectWallet(api)
     //setLucid(lucid)
     return lucid;
